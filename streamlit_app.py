e5e3fc2076e42b33e990fd8d7f8776e404677d09
chirag-9121 <chirag9121.cg@gmail.com>
2023-05-18 09:05:55 +0100

    integrated google drive api

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,73 @@
-from collections import namedtuple
-import altair as alt
-import math
+# Importing required libraries
+
 import pandas as pd
+import tensorflow as tf
+import seaborn as sns
+import matplotlib.pyplot as plt
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
 import streamlit as st
 
+st.set_page_config(layout="wide")
+st.markdown("<center><font size=6><b>MICI, The Image Classifier</b></font></center>",unsafe_allow_html=True)
+
 """
-# Image Classifier Model
+### Introduction
+
+Meet MICI (Machine Innovated to Classify Images), MICI is convolutional neural network model whose sole purpose is to 
+be able to distinguish between two images. In simple terms, it is like a baby who tries to find and arrange a set of patterns
+in order to distinguish between certain things. And just as a baby, its ability to classify images increases as it sees more and 
+more images of the same kind. Right now, MICI is in need of images to increase its classification ability. Will you help MICI 
+to get better? If yes, head on to the next section and follow the instructions.
 """
 
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-Point = namedtuple('Point', 'x y')
-data = []
-points_per_turn = total_points / num_turns
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+st.markdown("Hi, I am Chirag Gupta, the developer of MICI. I am a data science ethusiast who also interested in web development, game \
+development and designing. You can connect with me \
+on <a href='https://www.linkedin.com/in/chirag-gupta-359593218/'>LinkedIn</a>.",unsafe_allow_html=True)
 
-with st.echo(code_location='below'):
-    s = "dummy text"
+"""
+### Training MICI
+
+1. All you have to do is create a folder in your google drive and name it anything you like. 
+
+2. Inside that folder, create two sub folders that will contain images of two different entities.
+(A minimum of 50 images per entity is recommended)
+
+3. Make sure to name these folders according to the images that is stored in them.
+
+4. Once you're done with this, enter the name of your main folder in the input box below and hit submit. 
+
+5. MICI will then ask you permission to read the main folder. Grant the permission and wait for MICI to learn about the 
+newly provided data.
+"""
+
+st.markdown("***")
+folder_name = st.text_input("Name of the main folder:")
+st.markdown("<br>", unsafe_allow_html=True)
+
+if st.button("Submit"):
+    if not folder_name:
+        st.error("Please enter the folder name first")
+
+    else:
+        # Configuring the API
+
+        scope = ['https://www.googleapis.com/auth/drive.readonly']
+        credentials_path = 'credentials.json'
+
+        # Requesting access from user
+        flow = InstalledAppFlow.from_client_secrets_file(credentials_path, scopes=scope)
+        creds = flow.run_local_server(port=0)
+
+        # Building the service
+        service = build('drive', 'v3', credentials=creds, cache_discovery=False)
+
+        # Retrieving the files
+        results = service.files().list(q="mimeType='application/vnd.google-apps.folder' and name='{}'".format(folder_name), pageSize=10).execute()
+        items = results.get('files', [])
+        if items:
+            print('Files:')
+            for item in items:
+                print(f'{item["name"]} ({item["id"]})')
+        else:
+            print('No files found.')
